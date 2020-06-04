790cc29571b1e4f506559800dc378ce85e585bee
Guillaume Millot <guillaume.millot@gmail.com>
2020-06-04 19:17:07 +0200

    Update test.c

--- test.c
+++ test.c
@@ -2,7 +2,8 @@
 
 #include "library.h"
 
-int main(void)
+// Passes with 2GB (TIS_KERNEL_MAX_MEM=1.71)
+int test1(void)
 {
     int input = 3;
     int output;
@@ -12,6 +13,21 @@ int main(void)
     printf("%d", output);
     
     for (unsigned long i = 0 ; i < 200000; i++) {};
+    
+    return 0;
+}
+
+// Fails with 4GB (TIS_KERNEL_MAX_MEM=3.42)
+int test2(void)
+{
+    int input = 3;
+    int output;
+    
+    output = add(input);
+    
+    printf("%d", output);
+    
+    for (unsigned long i = 0 ; i < 400000; i++) {};
     
     return 0;
 }
